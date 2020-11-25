8d13442ba2a052887f37bf3b67d30917e514419b
victor-depre <59604992+victor-depre@users.noreply.github.com>
2020-11-25 21:51:39 +0100

    Rendement net de charges ajouté

--- CalculsInvestissementsLocatifs/App.R
+++ CalculsInvestissementsLocatifs/App.R
@@ -1,9 +1,12 @@
 library(shiny)
 library(shinythemes)
+
 # Define UI 
 
 ui = fluidPage(theme = shinytheme("cerulean"),
-     pageWithSidebar(
+     navbarPage("Calculs Investissements Locatifs",
+     tabPanel("Rendement Brut",
+     sidebarLayout(
   
   # App title
   headerPanel("Calculs Investissements Locatifs"),
@@ -19,13 +22,36 @@ ui = fluidPage(theme = shinytheme("cerulean"),
                  value = 0),
     numericInput(inputId = "P",
                  label = "Prix du bien :",
-                 value = 0),
-),
+                 value = 0)
+    ),
   
   # Main panel for displaying outputs
-  mainPanel(
-    h3(textOutput("RendementBrut")))
+  mainPanel(h3(textOutput("RendementBrut")))
+     )
+  ),
+  
 
+tabPanel("Rendement net de frais et charges",
+  sidebarLayout(
+    sidebarPanel(
+      h2("Calcul du rendement net de frais et charges"),
+      br(),
+      numericInput(inputId = "La2",
+                   label = "Loyer annuel",
+                   value = 0),
+      numericInput(inputId = "P2",
+                   label = "Prix du bien",
+                   value = 0),
+      numericInput(inputId = "FC2",
+                   label = "Frais et Charges (annuelles)",
+                   value = 0),
+      numericInput(inputId = "Cc2",
+                   label = "Coût du crédit",
+                   value = 0)
+    ),
+    mainPanel(h3(textOutput("RendementNetFraisCharges")))
+
+))
 ))
 
 # The three functions headerPanel, sidebarPanel, and mainPanel define the various regions of the user-interface. The application will be called “Calculs Investissements Locatifs” so we specify that as the title when we create the header panel. The other panels are empty for now.
@@ -39,7 +65,14 @@ server <- function(input, output, session) {
     results <- format(round(results, 2), nsmall = 2)
     paste("Le rendement brut s'élève à ", results, "%")
   })
+
+    output$RendementNetFraisCharges <- renderText({
+    results <- (input$La2-input$FC2)/(input$P2+input$Cc2)* 100
+    results <- format(round(results, 2), nsmall = 2)
+    paste("Le rendement net de frais et de charges s'élève à ", results, "%")
+  })
 }
+
 
 # Create Shiny App with ui and server
 
